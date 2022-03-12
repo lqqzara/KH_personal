2d857e5cdba395bcf95170feb30056c4a707c085
조아라 <mxnx0429@naver.com>
2022-03-12 17:48:38 +0900

    자바 기본 구조

---
+++ test001/src/test001/Example001.java
@@ -0,0 +1,32 @@
+package test001;
+
+//기능 추가
+import java.util.*;
+
+public class Example001 {
+
+//	멤버변수
+	private int number=0;
+	private String name="ara";
+	
+//	생성자: 클래스명과 같은 이름을 사용하는 메서드, 리턴타입이 없다
+	public Example001() {
+//		-자바에서 클래스를 생성할 때는 반드시 객체를 생성한다. 이 때 객체를 생성할 때 필요한 것이 생성자이다.
+//		-현재 이 생성자는 기본생성자로 클래스에 작성을 하지 않아도 기본적으로 생성되어 사용됨.
+		
+	}
+	
+//	멤버 메서드: 프로그램 동작을 위한 실행 코드 정의
+	public void method() {
+		
+//		지역변수: 특정 지역 안에서만 사용되는 변수
+		int i=0;
+		
+	}
+	
+//	메인 메서드(함수): 자바 프로그램에서 실행하기 위한 시작점으로 사용
+	public static void main(String[] args) {
+
+	}
+
+}
